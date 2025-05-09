18061d2afecfb5741a70c22e3db5a0e1b1b8c35e
Ibrahim <useinovski.iu@gmail.com>
2025-05-09 07:28:43 -0500

    Trying a new fix for the images not loading, took out the . and / before the file path

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -3,9 +3,9 @@ import AboutCarousel from "./AboutCarousel";
 const About = () => {
 
     const profileImages = [
-        "./iconLogo1.png", 
-        "./iconLogo2.png", 
-        "./iconLogo3.png"
+        "iconLogo1.png", 
+        "iconLogo2.png", 
+        "iconLogo3.png"
         
       ];
 
